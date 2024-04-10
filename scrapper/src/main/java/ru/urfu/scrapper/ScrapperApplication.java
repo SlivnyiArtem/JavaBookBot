89cc29cd0d46d3172bb5363d1604e0eae6f2f72d
lenurseytasanov <lenur.seytasanov@mail.ru>
2024-04-10 22:02:49 +0500

    add basic configuration

--- scrapper/src/main/java/ru/urfu/scrapper/ScrapperApplication.java
+++ scrapper/src/main/java/ru/urfu/scrapper/ScrapperApplication.java
@@ -2,8 +2,9 @@ package ru.urfu.scrapper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class ScrapperApplication {
     public static void main(String[] args) {
         SpringApplication.run(ScrapperApplication.class, args);
