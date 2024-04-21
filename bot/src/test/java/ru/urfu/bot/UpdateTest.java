03441b5a814f8df2807b5d6c4b7cc3f5c6eb63f9
lenurseytasanov <lenur.seytasanov@mail.ru>
2024-04-21 23:00:50 +0500

    add update test

--- bot/src/test/java/ru/urfu/bot/UpdateTest.java
+++ bot/src/test/java/ru/urfu/bot/UpdateTest.java
@@ -1,6 +1,5 @@
 package ru.urfu.bot;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -9,87 +8,87 @@ import org.telegram.telegrambots.meta.api.objects.Chat;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.urfu.bot.app.port.UserMessageProcessor;
-import ru.urfu.bot.domain.entities.Book;
 import ru.urfu.bot.domain.handlers.Command;
-import ru.urfu.bot.domain.handlers.books.AddBookCommand;
-import ru.urfu.bot.domain.handlers.bot.HelpBotCommand;
-import ru.urfu.bot.domain.services.UserBookService;
 
 import java.util.Map;
-import java.util.NoSuchElementException;
 
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.mock;
 
 public class UpdateTest {
-    String username;
-    long chatId = 1;
-    Message message;
-    Chat chat;
-    String text;
-    UserBookService userBookService;
-    Command command;
-    UserMessageProcessor userMessageProcessor;
-    Update update;
 
+    Map<String, Command> commandMap;
+    UserMessageProcessor userMessageProcessor;
 
 
     @BeforeEach
     void init(){
+        Command command1 = mock(Command.class);
+        Mockito.when(command1.supports(any())).thenReturn(false);
+        Command command2 = mock(Command.class);
+        Mockito.when(command2.supports(any())).thenReturn(true);
+        Mockito.when(command2.handle(any())).thenReturn(mock(SendMessage.class));
+        commandMap = Map.of(
+                "/command1", command1,
+                "/command2", command2
+        );
+        userMessageProcessor = new UserMessageProcessor(commandMap);
 
-        command = mock(Command.class);
-        update = mock(Update.class);
-        username = "username";
-        chat = mock(Chat.class);
-        message = mock(Message.class);
-        userBookService = mock(UserBookService.class);
+    }
 
+    private Update mockUpdate(String text) {
+        String username = "username";
+        Long chatId = 1L;
+        Update update = mock(Update.class);
+        Chat chat = mock(Chat.class);
+        Message message = mock(Message.class);
         Mockito.when(message.getChat()).thenReturn(chat);
         Mockito.when(message.getChatId()).thenReturn(chatId);
         Mockito.when(chat.getUserName()).thenReturn(username);
         Mockito.when(update.getMessage()).thenReturn(message);
         Mockito.when(message.getText()).thenReturn(text);
+        return update;
     }
 
     @Test
     void unknownCommandTest(){
-        //TODO
-//        Map<String, Command> commandMap = Map.ofEntries(Map.entry("/help", new HelpBotCommand(userBookService)));
-//        userMessageProcessor = mock(UserMessageProcessor.class, withSettings().useConstructor(commandMap));
-////        Mockito.when(userMessageProcessor.commands.get(command)).thenReturn(null);
-//
-//        text = "/unkn 1";
-//        String msgText = "Команда недоступна";
-//
-//        SendMessage expected_msg = new SendMessage();
-//        expected_msg.setChatId(update.getMessage().getChatId());
-//        expected_msg.setText(msgText);
-//
-//        SendMessage actual_msg = userMessageProcessor.process(update);
-//
-//        Assertions.assertEquals(expected_msg, actual_msg);
+        // Arrange
+        Update update = mockUpdate("/unkn 1");
+        String expected_msg = "Неизвестная команда. Введите /help для получения списка команд";
+
+        // Act
+        String actual_msg = userMessageProcessor.process(update).getText();
+
+        // Assert
+        assertEquals(expected_msg, actual_msg);
 
     }
 
     @Test
     void unsupportedCommandTest(){
-        //TODO
-////        userMessageProcessor = mock(UserMessageProcessor.class, withSettings().useConstructor(userService, searchService));
-//
-//        text = "/unsp 1";
-//        String text = "Неизвестная команда. Введите /help для получения списка команд";
-//
-//        SendMessage expected_msg = new SendMessage();
-//        expected_msg.setChatId(update.getMessage().getChatId());
-//        expected_msg.setText(text);
-//
-//        SendMessage actual_msg = userMessageProcessor.process(update);
-//
-//        Assertions.assertEquals(expected_msg, actual_msg);
+        // Arrange
+        Update update = mockUpdate("/command1");
+        String expected_msg = "Команда недоступна";
+
+        // Act
+        String actual_msg = userMessageProcessor.process(update).getText();
+
+        // Assert
+        assertEquals(expected_msg, actual_msg);
     }
 
     @Test
     void supportedCommandTest(){
-        //TODO
-//        verify(command).handle(update);
+        // Arrange
+        Update update = mockUpdate("/command2");
+        String expected_msg = "Команда недоступна";
+
+        // Act
+        String actual_msg = userMessageProcessor.process(update).getText();
+
+        // Assert
+        assertNotEquals(expected_msg, actual_msg);
     }
 }
